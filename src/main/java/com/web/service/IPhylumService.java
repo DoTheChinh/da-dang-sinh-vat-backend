1fadcd2896e95d6e2ba4434268ac4201e5337639
dongan2212 <dongan2212@gmail.com>
2018-05-05 23:57:54 +0700

    initial commit

---
+++ src/main/java/com/web/service/IPhylumService.java
@@ -0,0 +1,24 @@
+package com.web.service;
+
+import com.web.entity.backend.Phylum;
+
+import java.util.List;
+
+/**
+ * Created by duyle on 14/02/2017.
+ */
+public interface IPhylumService {
+
+    List<Phylum> getAllPhylums();
+
+    Phylum getPhylumById(int id);
+
+    int addPhylum(Phylum phylum);
+
+    void updatePhylum(Phylum phylum);
+
+    boolean isPhylumExistByPhylum(Phylum phylum);
+
+    boolean isPhylumExistByScienceNameVietnameseName(String scinceName, String vietnameseName);
+
+}
